044d355976407d008ae8b6c1e96e354a3046133b
novakvova <novakvova@gmail>
2023-01-13 11:17:25 +0200

    Working get products

--- laravel.pu012.com/app/Http/Controllers/API/ProductController.php
+++ laravel.pu012.com/app/Http/Controllers/API/ProductController.php
@@ -6,31 +6,46 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Api Project",
+ *      description="Demo my Project ",
+ *      @OA\Contact(
+ *          email="admin@gmail.com"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ *
+ * @OA\Get(
+ *     tags={"Product"},
+ *     path="/api/products",
+ *     @OA\Parameter(
+ *      name="page",
+ *      in="query",
+ *      required=true,
+ *      @OA\Schema(
+ *           type="string"
+ *      )
+ *   ),
+ *     @OA\Parameter(
+ *      name="name",
+ *      in="query",
+ *      required=false,
+ *      @OA\Schema(
+ *           type="string"
+ *      )
+ *   ),
+ *     @OA\Response(response="200", description="List Products.")
+ * )
+ */
+
 class ProductController extends Controller
 {
-    /**
-     * @OA\Get(
-     *     tags={"Product"},
-     *     path="/api/products",
-     *     @OA\Parameter(
-     *      name="page",
-     *      in="query",
-     *      required=true,
-     *      @OA\Schema(
-     *           type="string"
-     *      )
-     *   ),
-     *     @OA\Parameter(
-     *      name="name",
-     *      in="query",
-     *      required=false,
-     *      @OA\Schema(
-     *           type="string"
-     *      )
-     *   ),
-     *     @OA\Response(response="200", description="List Products.")
-     * )
-     */
+
     /**
      * Display a listing of the resource.
      *
--- laravel.pu012.com/app/Http/Controllers/Controller.php
+++ laravel.pu012.com/app/Http/Controllers/Controller.php
@@ -7,42 +7,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @OA\Info(
- *      version="1.0.0",
- *      title="Api Project",
- *      description="Demo my Project ",
- *      @OA\Contact(
- *          email="admin@gmail.com"
- *      ),
- *     @OA\License(
- *         name="Apache 2.0",
- *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
- *     )
- * )
- *
- * @OA\Get(
- *     tags={"Product"},
- *     path="/api/products",
- *     @OA\Parameter(
- *      name="page",
- *      in="query",
- *      required=true,
- *      @OA\Schema(
- *           type="string"
- *      )
- *   ),
- *     @OA\Parameter(
- *      name="name",
- *      in="query",
- *      required=false,
- *      @OA\Schema(
- *           type="string"
- *      )
- *   ),
- *     @OA\Response(response="200", description="List Products.")
- * )
- */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;

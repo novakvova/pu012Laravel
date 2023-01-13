549f64b8aa69cab06dd79e98a147893deac8400e
novakvova <novakvova@gmail>
2023-01-13 10:04:27 +0200

    Begin working search

--- my-app/src/components/home/HomePage.tsx
+++ my-app/src/components/home/HomePage.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { useFormik } from "formik";
 import qs from "qs";
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
@@ -16,13 +17,13 @@ const HomePage = () => {
 
   const [search, setSearch] = useState<ISearchProduct>({
     name: searchParams.get("name") || "",
-    page: searchParams.get("page") || 1
+    page: searchParams.get("page") || 1,
   });
 
   function filterNonNull(obj: ISearchProduct) {
     return Object.fromEntries(Object.entries(obj).filter(([k, v]) => v));
   }
-  
+
   //console.log("Search Product params", search);
 
   useEffect(() => {
@@ -45,18 +46,58 @@ const HomePage = () => {
 
   const pagination = buttons.map((page) => (
     <li key={page} className="page-item">
-      <Link className={classNames("page-link", {active: page===current_page})} 
-        onClick={()=>{ setSearch({...search, page: page} ) }}
-        to={"?"+qs.stringify(filterNonNull({...search, page: page}))}>
+      <Link
+        className={classNames("page-link", { active: page === current_page })}
+        onClick={() => {
+          setSearch({ ...search, page: page });
+        }}
+        to={"?" + qs.stringify(filterNonNull({ ...search, page: page }))}
+      >
         {page}
       </Link>
     </li>
   ));
 
+  const onSubmit = (values: ISearchProduct) => {
+    setSearchParams(qs.stringify(filterNonNull(values)));
+    setSearch(values);
+  };
+
+  const formik = useFormik({
+    initialValues: search,
+    onSubmit,
+  });
+
   return (
     <>
       <h1 className="text-center">Головна сторінка</h1>
       <div className="table-responsive">
+        <form
+          className=" w-100 mt-3 d-flex flex-wrap border border-secondary rounded-3 position-relative"
+          onSubmit={formik.handleSubmit}
+        >
+          <div className="mb-3 p-3 w-25">
+            <label htmlFor="name" className="form-label">
+              Назва
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={formik.handleChange}
+              className="form-control"
+              placeholder="пошук по імені"
+            />
+          </div>
+
+          <button type="submit" className="btn mb-3 btn-secondary">
+            <span>
+              <i className="fa fa-search"></i>
+            </span>
+            <span>Пошук</span>
+          </button>
+        </form>
+
         <h4>Всього записів: {total}</h4>
         <table className="table">
           <thead>
@@ -70,9 +111,7 @@ const HomePage = () => {
         </table>
 
         <nav>
-          <ul className="pagination">
-            {pagination}
-          </ul>
+          <ul className="pagination">{pagination}</ul>
         </nav>
       </div>
     </>
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
+import "font-awesome/css/font-awesome.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";

aa4c96f8b531a467fa75f572cbce121b0cf5fd08
novakvova <novakvova@gmail>
2023-01-18 10:05:07 +0200

    Begin working crud porduct

--- laravel.pu012.com/app/Http/Controllers/API/ProductController.php
+++ laravel.pu012.com/app/Http/Controllers/API/ProductController.php
@@ -5,111 +5,237 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
-
-/**
- * @OA\Info(
- *      version="1.0.0",
- *      title="Api Project",
- *      description="Demo my Project ",
- *      @OA\Contact(
- *          email="admin@gmail.com"
- *      ),
- *     @OA\License(
- *         name="Apache 2.0",
- *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
- *     )
- * )
- *
- * @OA\Get(
- *     tags={"Product"},
- *     path="/api/products",
- *     @OA\Parameter(
- *      name="page",
- *      in="query",
- *      required=true,
- *      @OA\Schema(
- *           type="string"
- *      )
- *   ),
- *     @OA\Parameter(
- *      name="name",
- *      in="query",
- *      required=false,
- *      @OA\Schema(
- *           type="string"
- *      )
- *   ),
- *     @OA\Response(response="200", description="List Products.")
- * )
- */
+use Validator;
 
 class ProductController extends Controller
 {
-
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *     tags={"Product"},
+     *     path="/api/products",
+     *     @OA\Parameter(
+     *      name="page",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *     @OA\Parameter(
+     *      name="name",
+     *      in="query",
+     *      required=false,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *     @OA\Parameter(
+     *      name="page_size",
+     *      in="query",
+     *      required=false,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *     @OA\Response(response="200", description="List Products.")
+     * )
      */
     public function index(Request $request)
     {
         $input = $request->all();
         $name = isset($input['name']) ? $input["name"] : "";
-        if(!empty($name))
-        {
-            $prodcuts = Product::where('name', 'LIKE', "%$name%")->paginate(2);
-            return response()->json($prodcuts,  200,
-                ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
-                JSON_UNESCAPED_UNICODE);
-        }
-        $prodcuts = Product::paginate(2);
-        return response()->json($prodcuts,  200,
+        $page_size = isset($input['page_size']) ? $input["page_size"] : 2;
+
+        $prodcuts = Product::where('name', 'LIKE', "%$name%")->paginate($page_size);
+        return response()->json($prodcuts, 200,
             ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
             JSON_UNESCAPED_UNICODE);
+
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\Post(
+     ** path="/api/products",
+     *   tags={"Product"},
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+     *
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Create product info",
+     *    @OA\JsonContent(
+     *       required={"name","detail"},
+     *       @OA\Property(property="name", type="string"),
+     *       @OA\Property(property="detail", type="string"),
+     *    ),
+     * ),
+     *   @OA\Response(
+     *      response=200,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *)
+     **/
     public function store(Request $request)
     {
-        //
+        $input = $request->all();
+        $product = Product::create($input);
+        return response()->json([
+            "success" => true,
+            "message" => "Product created",
+            "data"=> $product
+        ]);
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *     tags={"Product"},
+     *     path="/api/products/{id}",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Buscar por estado",
+     *         required=true,
+     *      ),
+     *     @OA\Response(response="200", description="List Products.")
+     * )
      */
-    public function show(Product $product)
+    public function show($id)
     {
-        //
+        $product = Product::find($id);
+        if (is_null($product)) {
+            return response()->json([
+                "success" => false
+            ], 404);
+        }
+        return response()->json([
+            "success" => true,
+            "message" => "Product retrieved successfully.",
+            "data" => $product
+        ]);
     }
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Put (
+     ** path="/api/products",
+     *   tags={"Product"},
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Product $product)
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Create product info",
+     *    @OA\JsonContent(
+     *       required={"id","name","detail"},
+     *       @OA\Property(property="id", type="integer"),
+     *       @OA\Property(property="name", type="string"),
+     *       @OA\Property(property="detail", type="string"),
+     *    ),
+     * ),
+     *   @OA\Response(
+     *      response=200,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *)
+     **/
+    public function update(Request $request)
     {
-        //
+        $input = $request->all();
+        $id = $input['id'] ?? 0;
+        $product = Product::find($id);
+        if (is_null($product)) {
+            return response()->json([
+                "success" => false
+            ], 404);
+        }
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                "Validation Error" => $validator->errors()
+            ], 404);
+        }
+        $product->name = $input['name'];
+        $product->detail = $input['detail'];
+        $product->save();
+        return response()->json([
+            "success" => true,
+            "message" => "Product updated successfully.",
+            "data" => $product
+        ]);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete  (
+     ** path="/api/products/{id}",
+     *   tags={"Product"},
      *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
+     *  @OA\Parameter(
+     *      name="id",
+     *      in="path",
+     *      description="Buscar por estado",
+     *      required=true,
+     *   ),
+     *   @OA\Response(
+     *      response=200,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *)
+     **/
+    public function destroy($id)
     {
-        //
+        $product = Product::find($id);
+        if (is_null($product)) {
+            return $this->sendError('Product not found.');
+        }
+        $product->delete();
+        return response()->json([
+            "success" => true,
+            "message" => "Product deleted successfully.",
+            "data" => $product
+        ]);
     }
 }
